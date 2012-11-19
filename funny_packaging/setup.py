1aba320a55b64e014a2bc7ab9006ec4ca93e20f1
patrick <ndkpatt@gmail.com>
2012-11-19 03:33:20 +0000

    joke function ready, setup ready

---
+++ funny_packaging/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funny_packaging',
+      version='0.1',
+      description='learning python packaging',
+      url='https://github.com/LeMeteore/furry-octo-ninja/tree/master/funny_packaging',
+      author='lemeteore',
+      author_email='patrick@soon.pro',
+      license='MIT',
+      packages=['funny_packaging'],
+      zip_safe=False)
